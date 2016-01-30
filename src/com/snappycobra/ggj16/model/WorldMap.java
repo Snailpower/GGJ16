732938981f45eb7674da3e167e203f36c2edf2a6
Lukeslux <lucas.brnd@gmail.com>
2016-01-30 18:03:45 +0100

    probleempje weg

--- src/com/snappycobra/ggj16/model/WorldMap.java
+++ src/com/snappycobra/ggj16/model/WorldMap.java
@@ -1,5 +1,6 @@
 package com.snappycobra.ggj16.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.snappycobra.motor.maps.GameObject;
@@ -9,6 +10,7 @@ public class WorldMap {
 	private List<ResourcePoint> resourcePointList;
 	
 	public WorldMap(List<GameObject> resources) {
+		this.resourcePointList = new ArrayList<ResourcePoint>();
 		for (GameObject res : resources) {
 			resourcePointList.add((ResourcePoint) res);
 		}
