dcea183662964d7ee7409a10c3edd390b06edc5f
Lukeslux <lucas.brnd@gmail.com>
2016-01-30 09:47:27 +0100

    Began new paint system

--- src/com/snappycobra/ggj16/Main.java
+++ src/com/snappycobra/ggj16/Main.java
@@ -4,13 +4,16 @@ import com.snappycobra.ggj16.graphics.GodPainter;
 import com.snappycobra.ggj16.model.GodGame;
 import com.snappycobra.motor.demo.DemoGame;
 import com.snappycobra.motor.demo.DemoPainter;
-import com.snappycobra.motor.graphics.GamePainter;
+import com.snappycobra.motor.demo.WindowCloseController;
 import com.snappycobra.motor.gui.Window;
 
 public class Main {
 	public static void main(String[] args){
-		GamePainter painter = new GodPainter(new GodGame());
-		Window window = new Window(painter);
+		GodPainter painter = new GodPainter(new GodGame());
+		Window window = new Window(300,300,painter);
+		window.addKeyListener(new WindowCloseController());
+		window.setResizable(true);
+		painter.showFPS(true);
 		window.setTitle("GGJ16");
 		window.setVisible(true);
 	}
